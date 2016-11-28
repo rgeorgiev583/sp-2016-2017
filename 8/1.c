18ccdac77f3ea724e9db7c71404c1f827fe942c4
Radoslav Georgiev <rgeorgiev583@gmail.com>
2016-11-28 10:07:01 +0200

    added solution to the first task from the eight meeting

---
+++ 8/1.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+
+int main()
+{
+    int fd[2];
+    pipe(fd);
+
+    if (!fork())
+    {
+        close(0);
+        close(fd[1]);
+        dup(fd[0]);
+        execlp("wc", "wc", "-l", NULL);
+    }
+    else
+    {
+        close(1);
+        close(fd[0]);
+        dup(fd[1]);
+        execlp("ls", "ls", NULL);
+    }
+
+    return 0;
+}
