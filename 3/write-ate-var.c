20cd1188d0ec2319ee15dce5789faffeb6501874
Radoslav Georgiev <rgeorgiev583@gmail.com>
2017-01-28 16:44:21 +0200

    3: made the examples more generic

--- 3/write-ate-var.c
+++ 3/write-ate-var.c
@@ -1,15 +1,15 @@
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
 
-
 int main(int argc, char** argv)
 {
-    if (argc < 3)
+    if (argc < 4)
         return 1;
 
     int fd = open(argv[1], O_WRONLY);
-    lseek(fd, 0, SEEK_END);  // премества офсета на позицията на първия байт след края на файла
+    lseek(fd, atoi(argv[3]), SEEK_END);  // премества офсета на позицията на първия байт след края на файла
     const char* str = argv[2];  // завършва с терминиращ нулев байт
     write(fd, str, strlen(str));
     return 0;
