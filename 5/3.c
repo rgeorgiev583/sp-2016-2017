4d9ac215a4d7395bf5b0ca232a3ca0d275a2cbbc
Radoslav Georgiev <rgeorgiev583@gmail.com>
2017-01-31 01:32:26 +0200

    5: added a command-line argument count check to the solutions of the third and fourth tasks

--- 5/3.c
+++ 5/3.c
@@ -3,5 +3,8 @@
 
 int main(int argc, char** argv)
 {
+    if (argc < 2)
+        return 1;
+
     execvp(argv[1], argv + 1);
 }
--- 5/4.c
+++ 5/4.c
@@ -6,12 +6,15 @@ int main(int argc, char** argv)
 {
     argv[0] = "wc";
 
-    if (!strcmp(argv[1], "chars"))
-        argv[1] = "-c";
-    else if (!strcmp(argv[1], "words"))
-        argv[1] = "-w";
-    else if (!strcmp(argv[1], "lines"))
-        argv[1] = "-l";
+    if (argc > 2)
+    {
+        if (!strcmp(argv[1], "chars"))
+            argv[1] = "-c";
+        else if (!strcmp(argv[1], "words"))
+            argv[1] = "-w";
+        else if (!strcmp(argv[1], "lines"))
+            argv[1] = "-l";
+    }
 
     execvp(argv[0], argv);
 }
