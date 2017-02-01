bf0a7a933258ba6b364557587f8d0efa9a77689e
Radoslav Georgiev <rgeorgiev583@gmail.com>
2017-02-01 13:54:32 +0200

    kr: added solution to the second task

---
+++ kr/2.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include <fcntl.h>
+
+int main(int argc, char** argv)
+{
+    if (argc < 5)
+        return 1;
+
+    close(0);
+    open(argv[3], O_RDONLY);
+    close(1);
+    creat(argv[4], 0644);
+    execlp("tr", "tr", argv[1], argv[2], NULL);
+}
