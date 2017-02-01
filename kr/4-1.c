ef8dd7ffc62a645a9da1774cfde088aba6345829
Radoslav Georgiev <rgeorgiev583@gmail.com>
2017-02-01 15:34:41 +0200

    kr: refactored the solution to task 4.1. a bit: linearized code

--- kr/4-1.c
+++ kr/4-1.c
@@ -9,53 +9,53 @@ int main(int argc, char** argv)
     if (!fork())
     {
         close(fds1[0]);
+
         close(1);
         dup(fds1[1]);
+
         argv[0] = "grep";
         execvp(argv[0], argv);
     }
-    else
+
+    close(fds1[1]);
+
+    int fds2[2];
+    pipe(fds2);
+
+    if (!fork())
     {
-        int fds2[2];
-        pipe(fds2);
+        close(fds2[0]);
 
-        close(fds1[1]);
+        close(0);
+        dup(fds1[0]);
+        close(1);
+        dup(fds2[1]);
 
-        if (!fork())
-        {
-            close(fds2[0]);
-            close(0);
-            dup(fds1[0]);
-            close(1);
-            dup(fds2[1]);
-            execlp("sort", "sort", NULL);
-        }
-        else
-        {
-            int fds3[2];
-            pipe(fds3);
-
-            close(fds1[0]);
-            close(fds2[1]);
-
-            if (!fork())
-            {
-                close(fds3[0]);
-                close(0);
-                dup(fds2[0]);
-                close(1);
-                dup(fds3[1]);
-                execlp("uniq", "uniq", NULL);
-            }
-            else
-            {
-                close(fds3[1]);
-                close(0);
-                dup(fds3[0]);
-                execlp("wc", "wc", "-l", NULL);
-            }
-        }
+        execlp("sort", "sort", NULL);
     }
 
-    return 0;
+    close(fds1[0]);
+    close(fds2[1]);
+
+    int fds3[2];
+    pipe(fds3);
+
+    if (!fork())
+    {
+        close(fds3[0]);
+
+        close(0);
+        dup(fds2[0]);
+        close(1);
+        dup(fds3[1]);
+
+        execlp("uniq", "uniq", NULL);
+    }
+
+    close(fds3[1]);
+
+    close(0);
+    dup(fds3[0]);
+
+    execlp("wc", "wc", "-l", NULL);
 }
